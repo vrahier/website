43f425bd384be36df48d767f6559b2778ff19197
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-03 17:38:23 -0400

    Removing useless Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Menu from './components/Menu';
 
 function App() {
 
