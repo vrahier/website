7e599b84cb8c6bfb3fcefcff9799fbbc51d5bca1
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-03 11:47:37 -0400

    Fetch content.json data in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { useEffect, useState } from 'react';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Menu from './components/Menu';
+
 function App() {
 
+  const [data, setData] = useState();
+
+  const fetchData = () => {
+    fetch('content.json',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    ).then((response) => {
+      return response.json();
+    }).then((jsonResponse) => {
+      setData(jsonResponse);
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <Header/>
-      <Menu/>
-      <Content/>
-      <Footer/>
+    <>{ data && <>
+          <Header/>
+          <Menu/>
+          <Content/>
+          <Footer/>
+        </>
+      }
     </>
   )
 }
