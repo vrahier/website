b44412f240c29bdc99dbfb3af93691a147f8a356
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-03 18:43:41 -0400

    Add person information and presentation to Content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,26 @@
 import { PropTypes } from 'prop-types';
+import './Content.css';
 
 function Content({ data }) {
-  return <></>
+  return (
+    <div className="contentDiv">
+      <div>
+        <h1>{data.firstName} {data.name}</h1>
+        <h2>{data.jobTitle}</h2>
+        <img className="picture" src="/picture.png"/>
+      </div>
+      <div id="presentation">
+        {data.presentation}
+      </div>
+    </div>
+  )
 }
 
 Content.propTypes = {
   data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    firstName: PropTypes.string.isRequired,
+    jobTitle: PropTypes.string,
     presentation: PropTypes.string,
     experiences: PropTypes.array.isRequired,
     skills: PropTypes.array.isRequired,
