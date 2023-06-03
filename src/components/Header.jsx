b2601be7553e6efc5fc67155fa5158ca2745597b
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-03 17:08:00 -0400

    Define header component
    The header is fixed on top of the page and displays a few links to
    navigate in website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,17 @@
+import './Header.css'
+
 function Header() {
-  return <></>
+
+  const menuItems = ["Home", "About", "Resume", "Contact"];
+
+  return (
+    <header>
+        <ul className="menuList">
+        { menuItems.map((item, index) => (
+          <li key={index} className="menuItem">{item}</li>
+        ))}
+        </ul>
+    </header>
+  )
 }
 export default Header;
