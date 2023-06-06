78e98773a7ceecebf2c8b25e707ce731cc6bd303
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-06 15:03:43 -0400

    Always display description of experiences on medium & large screen

--- src/components/Experience/Style.js
+++ src/components/Experience/Style.js
@@ -9,15 +9,23 @@ export const Container = styled.div`
 export const Description = styled.div`
   display: ${props => props.show ? "block": "none"};
   text-align: justify;
+
+  @media only screen and (min-width: 641px) {
+    display: block;
+  }
 `;
 
 export const MoreButton = styled.button`
-   background: transparent;
-   border: 0;
-   width: fit-content;
-   align-self: center;
+  background: transparent;
+  border: 0;
+  width: fit-content;
+  align-self: center;
 
-   &:hover {
-      color: ${colors.accent}
-   }
+  &:hover {
+    color: ${colors.accent}
+  }
+
+  @media only screen and (min-width: 641px) {
+    display: none;
+  }
 `;
