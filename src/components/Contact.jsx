0db8329ea252412da3168647987f6f35ae3a3dcb
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-08 15:08:28 -0400

    Adding ref to contact links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact({firstName, name, email, links}){
     <p><a href={"mailto:" + email}>{email}</a></p>
     <div>
       {links
-        .map((l, index) => <a key={index} href={l.url} target="_blank">{l.name}</a>)
+        .map((l, index) => <a key={index} href={l.url} target="_blank" rel="noreferrer">{l.name}</a>)
         .reduce((prev, curr) => [prev, " / ", curr])
       }
     </div>
