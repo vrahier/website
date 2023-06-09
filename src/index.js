81b0b82ff86ca9a6225e42b170ab90096ee59e6d
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-09 16:05:32 -0400

    Load global style only where colors download is over

--- src/index.js
+++ src/index.js
@@ -10,8 +10,12 @@ import {GlobalStyle} from './utils/style/GlobalStyle';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    {colors && <App/>}
+    { colors &&
+      <>
+        <GlobalStyle />
+        <App />
+      </>
+    }
   </React.StrictMode>
 );
 
