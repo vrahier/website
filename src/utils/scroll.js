9bd8bd1c2435abca23a0d06cd90888fda4736407
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-09 19:26:48 -0400

    Scroll to top of the screen on click

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -2,7 +2,7 @@ export function scrollToDiv(id) {
     let header = document.getElementsByTagName('header')[0];
     let div = document.getElementById(id);
     window.scrollTo({
-      top: div.offsetTop - header.offsetHeight - window.innerHeight/10 ,
+      top: div.offsetTop ,
       behavior: "smooth"
     });
 }
