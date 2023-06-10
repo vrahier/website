1cfbe1f1764c5c00c5506f04e248ec300cdc0b59
Valentine RAHIER <rahiervalentine@gmail.com>
2023-06-09 20:07:21 -0400

    Remove unused var in scroll.js

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,5 +1,4 @@
 export function scrollToDiv(id) {
-    let header = document.getElementsByTagName('header')[0];
     let div = document.getElementById(id);
     window.scrollTo({
       top: div.offsetTop ,
